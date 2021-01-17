7dae5b5bce5bf77fc896ea2b40e99c6085f34bb8
Rainbrand <26818172+Rainbrand@users.noreply.github.com>
2021-01-18 01:48:51 +0500

    Added session ID to token payload

--- src/back/routers/apiTokenRouter.js
+++ src/back/routers/apiTokenRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 import JWTGenerator from "../jwtHandler.js"
+
 
 const apiTokenRouter = express.Router()
 
 apiTokenRouter.post('/token', (req, res) => {
     const username = req.username
     const jwt = new JWTGenerator()
-    const token = jwt.generate({username: username})
+    const token = jwt.generate({
+        username: username,
+        id: uuidv4()
+    })
     res.send({token}).status(201)
 })
 
